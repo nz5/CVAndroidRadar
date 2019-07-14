24d007f06f92f52b39270277083f68dbf1c2bc88
znizami <30432807+nz5@users.noreply.github.com>
2019-07-14 18:16:10 +0200

    initialize project

---
+++ app/src/main/java/com/nz/radar/MainActivity.java
@@ -0,0 +1,22 @@
+package com.nz.radar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (OpenCVLoader.initDebug()){
+            Toast.makeText(this, "opencv successfully loaded", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "opencv load failed", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
