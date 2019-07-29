c04252fc64131a8b6f5b20fff74c1b22cec18bea
znizami <30432807+nz5@users.noreply.github.com>
2019-07-30 00:44:48 +0200

    draw bounding boxes around found countors

--- app/src/main/java/com/nz/radar/CameraActivity.java
+++ app/src/main/java/com/nz/radar/CameraActivity.java
@@ -6,20 +6,22 @@ import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
 
-import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
-import org.opencv.android.JavaCamera2View;
 import org.opencv.android.JavaCameraView;
-import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -27,7 +29,7 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 //    private CameraBridgeViewBase cameraBridgeViewBase;
 
     JavaCameraView javaCameraView;
-    Mat mat1, mat2;
+    Mat mHsv, mMask, mOriginal, mat4, mGaussianBlue, mErode, mDilate;
     Scalar scalarLow, scalarHigh;
 
 //    Mat mRgba =  new Mat();
@@ -60,7 +62,7 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 //        cameraBridgeViewBase.setCvCameraViewListener(this);
 
         OpenCVLoader.initDebug();
-        javaCameraView =  findViewById(R.id.opencv_camera_view);
+        javaCameraView = findViewById(R.id.opencv_camera_view);
         javaCameraView.setVisibility(SurfaceView.VISIBLE);
 //        javaCameraView.setCameraIndex(0);
         javaCameraView.setCvCameraViewListener(this);
@@ -84,7 +86,7 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
     }
 
     @Override
-    public void onResume(){
+    public void onResume() {
         super.onResume();
         javaCameraView.enableView();
 //        if (!OpenCVLoader.initDebug()) {
@@ -95,12 +97,18 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 //            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
 //        }
     }
+
     @Override
     public void onCameraViewStarted(int width, int height) {
         Log.e(TAG, "@______@__________@________@________@camera started");
 
-        mat1 = new Mat(width, height, CvType.CV_16UC4);
-        mat2 = new Mat(width, height, CvType.CV_16UC4);
+        mHsv = new Mat(width, height, CvType.CV_8UC4);
+        mMask = new Mat(width, height, CvType.CV_8UC4);
+        mOriginal = new Mat(width, height, CvType.CV_8UC4);
+        mat4 = new Mat(width, height, CvType.CV_8UC4);
+        mGaussianBlue = new Mat(width, height, CvType.CV_8UC4);
+        mErode = new Mat(width, height, CvType.CV_8UC4);
+        mDilate = new Mat(width, height, CvType.CV_8UC4);
     }
 
     @Override
@@ -110,143 +118,49 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        mRgba =  inputFrame.rgba();
-//
-//        Imgproc.cvtColor(mRgba, mRgb, Imgproc.COLOR_RGBA2RGB);
-//
-//        Imgproc.cvtColor(mResult, mRgba, Imgproc.COLOR_BGR2RGBA);
 
-//        Imgproc.cvtColor(mRgba, mRgb, Imgproc.COLOR_RGBA2RGB);
-//        Imgproc.cvtColor(mRgb, mHsv, Imgproc.COLOR_RGB2HSV, 3);
-//
-//        Scalar lowerBound = new Scalar(11, 60, 44);
-//        Scalar upperBound = new Scalar(147, 218, 200);
-//        Core.inRange(mHsv, lowerBound, upperBound, mMask);
-//        Core.bitwise_and(mRgb, mRgb, mResult, mMask);
-//
-//        Imgproc.cvtColor(mResult, mRgba, Imgproc.COLOR_BGR2RGBA);
 
-//        return mRgba;
+        mOriginal = inputFrame.rgba();
 
-        Imgproc.cvtColor(inputFrame.rgba(), mat1, Imgproc.COLOR_BGR2HSV);
-        Core.inRange(mat1, scalarLow, scalarHigh, mat2);
+        Imgproc.GaussianBlur(mOriginal, mOriginal,new Size(15,15), 0);
+        Imgproc.cvtColor(mOriginal, mHsv, Imgproc.COLOR_BGR2HSV);
+        Core.inRange(mHsv, scalarLow, scalarHigh, mMask);
 
-        return mat2;
+        int erosion_size = 5;
+        int dilation_size = 5;
+        Mat strcuturingElementErode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(2*erosion_size + 1, 2*erosion_size+1));
+        Mat strcuturingElementDilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new  Size(2*dilation_size + 1, 2*dilation_size+1));
+        Imgproc.erode(mMask, mErode, strcuturingElementErode);
+        Imgproc.dilate(mErode, mDilate, strcuturingElementDilate);
+
+        List<MatOfPoint> contours = new ArrayList<>();
+        Imgproc.findContours(mDilate, contours, mat4, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        Log.w(TAG, "--------------------------contour length=" + contours.size());
+
+        int maxIndex = 0;
+        double maxArea = 0;
+        for (int i=0; i < contours.size(); i++) {
+            double tempArea = Imgproc.contourArea(contours.get(i));
+            if (tempArea > maxArea) {
+                maxArea = tempArea;
+                maxIndex = i;
+
+                //TODO find max of bounding box and draw only that
+                Rect rect = Imgproc.boundingRect(contours.get(i));
+                Imgproc.rectangle(mOriginal, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,255,0));
+            }
+//            if (Imgproc.contourArea(contours.get(i)) > 3 ){
+//                Rect rect = Imgproc.boundingRect(contours.get(i));
+//                Imgproc.rectangle(mOriginal, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),new Scalar(0,255,0));
+//            }
+        }
+
+//        Rect rect = Imgproc.boundingRect(contours.get(maxIndex));
+//        Imgproc.rectangle(mOriginal, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 0, 0));
+
+        return mOriginal;
     }
 
-//    public Mat skinDetection(Mat src) {
-//        // define the upper and lower boundaries of the HSV pixel
-//        // intensities to be considered 'skin'
-//        Scalar lower = new Scalar(0, 48, 80);
-//        Scalar upper = new Scalar(20, 255, 255);
-//
-//        // Convert to HSV
-//        Mat hsvFrame = new Mat(src.rows(), src.cols(), CvType.CV_8U, new Scalar(3));
-//        Imgproc.cvtColor(src, hsvFrame, Imgproc.COLOR_RGB2HSV, 3);
-//
-//        // Mask the image for skin colors
-//        Mat skinMask = new Mat(hsvFrame.rows(), hsvFrame.cols(), CvType.CV_8U, new Scalar(3));
-//        Core.inRange(hsvFrame, lower, upper, skinMask);
-////        currentSkinMask = new Mat(hsvFrame.rows(), hsvFrame.cols(), CvType.CV_8U, new Scalar(3));
-////        skinMask.copyTo(currentSkinMask);
-//
-//        // apply a series of erosions and dilations to the mask
-//        // using an elliptical kernel
-//        final Size kernelSize = new Size(11, 11);
-//        final Point anchor = new Point(-1, -1);
-//        final int iterations = 2;
-//
-//        Mat kernel = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, kernelSize);
-//        Imgproc.erode(skinMask, skinMask, kernel, anchor, iterations);
-//        Imgproc.dilate(skinMask, skinMask, kernel, anchor, iterations);
-//
-//        // blur the mask to help remove noise, then apply the
-//        // mask to the frame
-//        final Size ksize = new Size(3, 3);
-//
-//        Mat skin = new Mat(skinMask.rows(), skinMask.cols(), CvType.CV_8U, new Scalar(3));
-//        Imgproc.GaussianBlur(skinMask, skinMask, ksize, 0);
-//        Core.bitwise_and(src, src, skin, skinMask);
-//
-//        return skin;
-//    }
 
-//    private CameraBridgeViewBase mOpenCvCameraView;
-//
-//    static {
-//        if (OpenCVLoader.initDebug()){
-//            System.out.println("-----------------------------------yessss-----------------------------------");
-//        } else {
-//            System.out.println("-----------------------------------noooooo-----------------------------------");
-//        }
-//    }
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-//        setContentView(R.layout.activity_camera);
-//        mOpenCvCameraView = findViewById(R.id.opencv_camera_view);
-//        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-//        mOpenCvCameraView.setCvCameraViewListener(this);
-//    }
-//
-//    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
-//        @Override
-//        public void onManagerConnected(int status) {
-//            switch (status) {
-//                case LoaderCallbackInterface.SUCCESS:
-//                {
-//                    Log.i("TAG??", "OpenCV loaded successfully");
-//                    mOpenCvCameraView.enableView();
-//                } break;
-//                default:
-//                {
-//                    super.onManagerConnected(status);
-//                } break;
-//            }
-//        }
-//    };
-//
-//    @Override
-//    public void onPause()
-//    {
-//        super.onPause();
-//        if (mOpenCvCameraView != null)
-//            mOpenCvCameraView.disableView();
-//    }
-//
-//    public void onDestroy() {
-//        super.onDestroy();
-//        if (mOpenCvCameraView != null)
-//            mOpenCvCameraView.disableView();
-//    }
-//
-//    @Override
-//    public void onResume()
-//    {
-//        super.onResume();
-////        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
-//        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
-//    }
-//
-//    @Override
-//    public void onCameraViewStarted(int width, int height) {
-//
-//    }
-//
-//    @Override
-//    public void onCameraViewStopped() {
-//
-//    }
-//
-//    @Override
-//    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        return inputFrame.rgba();
-//    }
-//
-//    @Override
-//    public void onPointerCaptureChanged(boolean hasCapture) {
-//
-//    }
+
 }
