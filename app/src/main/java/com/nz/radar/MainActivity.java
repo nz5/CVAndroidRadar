d58d25ba9cf638d0902ef342ddf3b9f2db92e05b
znizami <30432807+nz5@users.noreply.github.com>
2019-09-14 15:06:52 +0200

    improve home screen: distance, direction, logo, background

--- app/src/main/java/com/nz/radar/MainActivity.java
+++ app/src/main/java/com/nz/radar/MainActivity.java
@@ -5,6 +5,10 @@ import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.nz.radar.recording.CameraAbsDiffActivity;
@@ -16,6 +20,15 @@ import org.opencv.android.OpenCVLoader;
  */
 public class MainActivity extends AppCompatActivity {
 
+    private static final int INIT_DISTANCE = 5;
+    private static final int MAX_DISTANCE = 5;
+
+    private SeekBar seekBarDistance;
+    private TextView seekBarText;
+
+    private Switch switchDirection;
+    public boolean isLeftToRight = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +39,43 @@ public class MainActivity extends AppCompatActivity {
         } else {
             Toast.makeText(this, "opencv load failed", Toast.LENGTH_SHORT).show();
         }
+
+        setUserInputs();
+    }
+
+    private void setUserInputs(){
+        seekBarDistance = findViewById(R.id.seekBar_distance);
+        seekBarDistance.setMax(15);
+        seekBarDistance.setProgress(INIT_DISTANCE);
+        seekBarText = findViewById(R.id.text_distance);
+        seekBarText.setText(INIT_DISTANCE + " meters");
+        seekBarDistance.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBarDistance.setProgress(progress);
+                seekBarText.setText(progress + " meters");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        switchDirection = findViewById(R.id.switch_dir);
+        isLeftToRight = switchDirection.isChecked();
+        switchDirection.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isLeftToRight = isChecked;
+                System.out.println("--------------- " + isChecked);
+            }
+        });
     }
 
     public void whenGoToCameraActivityButtonPressed(final View view) {
