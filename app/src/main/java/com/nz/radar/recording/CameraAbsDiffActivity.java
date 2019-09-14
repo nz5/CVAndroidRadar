cc3540f6f3c757bb8d0bc32fd78adb6855420a28
znizami <30432807+nz5@users.noreply.github.com>
2019-09-14 10:26:58 +0200

    initialize activity for absDiff mode

---
+++ app/src/main/java/com/nz/radar/recording/CameraAbsDiffActivity.java
@@ -0,0 +1,15 @@
+package com.nz.radar.recording;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.nz.radar.R;
+
+public class CameraAbsDiffActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_abs_diff);
+    }
+}
